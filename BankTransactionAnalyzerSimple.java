70ab4dec888e850f1841b3ef847861e3fcfec78d
Adeola23 <amosadeola92@gmail.com>
2023-04-14 21:42:03 +0100

    parseFromCSV and parseLinesFromCSV methods

--- BankTransactionAnalyzerSimple.java
+++ BankTransactionAnalyzerSimple.java
@@ -2,6 +2,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class BankTransactionAnalyzerSimple {
@@ -12,11 +15,21 @@ public class BankTransactionAnalyzerSimple {
         final List<String> lines = Files.readAllLines(path);
 
         double total = 0d;
+        double totalJan = 0d;
+        final DateTimeFormatter DATE_PATTERN = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         for (final String line : lines){
             final String[] columns = line.split(",");
             final double amount = Double.parseDouble(columns[1]);
+            final LocalDate date = LocalDate.parse(columns[0]);
+            if(date.getMonth() == Month.JANUARY){
+                final double amountJan = Double.parseDouble(columns[1]);
+                totalJan += amountJan;
+            }
             total += amount;
         }
+
+
+
 
         System.out.println("The total for all transaction is " + total);
 
