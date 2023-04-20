a58f729c564d88b0b001f3fc4c136154f2b3d63b
Adeola23 <amosadeola92@gmail.com>
2023-04-20 03:07:18 +0100

    bank transaction filter

--- src/main/java/src/main/java/MainApplication.java
+++ src/main/java/src/main/java/MainApplication.java
@@ -7,11 +7,13 @@ import src.main.java.BankStatementParser;
 import src.main.java.BankTransactionAnalyzerSimple;
 
 import java.io.IOException;
+import java.util.List;
 
 public class MainApplication {
     public  static  void main(final String... args) throws IOException{
         final BankTransactionAnalyzerSimple bankTransactionAnalyzer = new BankTransactionAnalyzerSimple();
         final BankStatementParser bankStatementParser = new BankStatementCSVParser();
         bankTransactionAnalyzer.analyze(args[0], bankStatementParser);
+
     }
 }
